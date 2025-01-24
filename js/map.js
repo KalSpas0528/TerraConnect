a3f95da1e7a1c329ab77832409711c33ce9b6efc
KalSpas0528 <kaloyan_kj@yahoo.com>
2025-01-24 19:59:53 +0000

    created the Map using openstreetmap

--- js/map.js
+++ js/map.js
@@ -1,7 +1,9 @@
-// Initialize the map
-const map = L.map('map').setView([20, 0], 2);
-
-// Add a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+document.addEventListener("DOMContentLoaded", () => {
+    const map = L.map("map").setView([0, 0], 2)
+  
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
+  })
+  
+  
