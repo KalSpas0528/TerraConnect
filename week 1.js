a228fe8678bcb7ad8fff317bab5ae08b07422b30
KalSpas0528 <kaloyan_kj@yahoo.com>
2025-01-24 20:16:54 +0000

    week 1 day 1

---
+++ week 1.js
@@ -0,0 +1,2 @@
+// Day 1
+//I am trying to create a place/”hub” where you can quickly find news from other countries in the most simple way possible. I am visualizing this “hub” through an interactive map.
