bd8c301ef3b461c5d25e537e847bd34fc2536e6e
KalSpas0528 <kaloyan_kj@yahoo.com>
2025-01-27 18:23:56 +0000

    week 3 Day 2

--- week 1.js
+++
@@ -1,2 +0,0 @@
-// Day 1
-//I am trying to create a place/”hub” where you can quickly find news from other countries in the most simple way possible. I am visualizing this “hub” through an interactive map.
---
+++ weeks.js
@@ -0,0 +1,10 @@
+// Day 1
+//I am trying to create a place/”hub” where you can quickly find news from other countries in the most simple way possible. I am visualizing this “hub” through an interactive map.
+// Day 2
+//I will not be using AI in my project, however I will have AI help me with my project(i.e create the map)
+        /*
+        Research APIs
+        Use Leaflet.js to create an interactive base map.
+        Link News API for fetching live updates.
+        Simplify map layers and connect all.
+        */
