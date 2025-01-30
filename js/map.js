e288031c5fb276d7c23d11ffea57be9a579d8d8d
KalSpas0528 <kaloyan_kj@yahoo.com>
2025-01-30 20:23:30 +0000

    changed language to english only

--- js/map.js
+++ js/map.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const map = L.map("map").setView([0, 0], 2)
-  
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  const map = L.map("map").setView([0, 0], 2);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map)
-  })
-  
-  
+  }).addTo(map);
+});
